a86ede593a3633c4f784db5b61f1d12095c89b56
Anthony <sean.a.protho1@gmail.com>
2022-09-23 18:14:25 -0700

    checkPermutation

--- Chapter 1. Hashtables - strings/1.isUnique.py
+++ Chapter 1. Hashtables - strings/1.isUnique.py
@@ -2,6 +2,11 @@
  string has all unique characters. What if
  you cannot use additional data structures?'''
 
+# h is a dictionary, a dictionary counts as a data structure
+# so we added a data structure making this solution invalid. 
+
+# Time Complexity: O(s)
+# Space Complexity: O(h)
 def isUniqueChars(s):
     h = {}
     for i in range(len(s)):
--- Chapter 1. Hashtables - strings/2.checkPermutation.py
+++ Chapter 1. Hashtables - strings/2.checkPermutation.py
@@ -1,9 +1,36 @@
 """Given two strings, write a method to
  decide if one is a permutation of the other."""
 
+# what is a permutation?
+# a permutation is a reordered set of characters
+# CBA BCA CAB BAC and ACB are permutations of ABC
+
+# Time Complexity: O(perm^2 + combinations) -> O(perm)
+# Space Complexity: O(combos)
+
+from itertools import permutations
+
 def checkP(string, perm):
-    if perm in string:
-        return True
+    # edge cases 
+    # include the string being the same
+    if perm == string:
+        return False
+    # or the length of strings differ
+    elif len(perm) != len(string):
+        return False
+
+    # create list of all combos of perm
+    combos = [''.join(p) for p in permutations(perm)]
+    print(combos)
+
+    #compare string with all combos
+    for i in combos:
+        # if a combo matches
+        if string == i:
+            # it's a permutation of string
+            return True
+
+    # if nothing matches, it's not a permutation
     return False
 
-print(checkP("the cat", "hec"))
+print(checkP("the cat", "hec tat"))
